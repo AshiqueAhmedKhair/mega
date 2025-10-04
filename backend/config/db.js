5eac7e42f1b5a9715c4e50770fa0c345a93432d2
Nure <nalamsiddiq@gmail.com>
2025-10-05 04:35:42 +0600

    Add Mega Project

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/product';
+import { MONGODB_URI } from './utils.js';
+
+// const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/product';
 
 async function connectDatabase() {
   try {
     await mongoose.connect(MONGO_URI);
     console.log('Database connection successful!!!!');
   } catch (error) {
-    console.log(`Database connection failed ${MONGO_URI}`, error);
+    console.log(`Database connection failed ${MONGODB_URI}`, error);
   }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,15 @@ const cors = require('cors');
 const express = require('express');
 const router = require('./routes/router');
 const productRouter = require('./routes/productRoutes');
-const connectDatabase = require('./db');
+const connectDatabase = require('./config/db');
+import { PORT } from './config/utils.js';
 
-const port = 5000;
+const port = PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(
-  cors({
-    // origin: 'http://localhost:3000',
-  })
+  cors()
 );
 
 connectDatabase();
